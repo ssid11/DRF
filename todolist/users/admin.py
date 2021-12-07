427dea2ef7cc7c50df8fddf3dccd49b7fc8a24ee
ssid <evp.alcor@yandex.ru>
2021-12-07 18:52:21 +0300

    lesson_6 Done

--- todolist/users/admin.py
+++ todolist/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
+from .models import User
+
+# Register your models here.
+
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name','email',)
+    list_display_links = ('username',)
+    search_fields = ('username', 'first_name', 'last_name', )
+
+admin.site.register(User, MyUserAdmin)
 # Register your models here.
--- todolist/users/management/commands/create_users.py
+++ todolist/users/management/commands/create_users.py
@@ -4,10 +4,10 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.all().delete()
-        User.objects.create_user(username='Test1',email='t1@a.com')
-        User.objects.create_user(username='Test2', email='t2@a.com')
-        User.objects.create_user(username='Test3', email='t3@a.com')
+        User.objects.create_user(username='admin',email='t1@a.com',password='1')
+        User.objects.create_user(username='dev', email='t2@a.com',password='1')
+        User.objects.create_user(username='owner', email='t3@a.com',password='1')
         User.objects.create_superuser(username='su',email='a@b.com',password='1')
-        for idx in range(4,200,1):
-            User.objects.create_user(username='Test' + str(idx), email='Test' + str(idx)+'@a.com')
+        # for idx in range(4,200,1):
+        #     User.objects.create_user(username='Test' + str(idx), email='Test' + str(idx)+'@a.com')
 
