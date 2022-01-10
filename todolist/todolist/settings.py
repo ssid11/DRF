e46124ccba0530adae672dd33b598d1e7e2efef3
ssid <evp.alcor@yandex.ru>
2022-01-10 10:22:48 +0300

    Correct static

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'frontend/build',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =(BASE_DIR/'frontend/build/static',)
+print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
