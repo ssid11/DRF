cf0d1fd727f3a2bc9c68846d57c29d6187f0f57f
ssid <evp.alcor@yandex.ru>
2022-01-10 19:44:53 +0300

    Move frontend

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'frontend/build',],
+        'DIRS': [BASE_DIR/'../frontend/build',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,8 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =(BASE_DIR/'frontend/build/static',)
-print(STATICFILES_DIRS)
+STATICFILES_DIRS =(BASE_DIR/'../frontend/build/static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
